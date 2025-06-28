7ccec24d83480cb7b618e0810e079299b332ff92
Halil Ozel <halilozel1903@gmail.com>
2025-06-28 16:19:13 +0300

    SuppressLint added for Models.kt

--- app/src/main/java/com/halil/ozel/pokemonapp/data/Models.kt
+++ app/src/main/java/com/halil/ozel/pokemonapp/data/Models.kt
@@ -1,19 +1,23 @@
 package com.halil.ozel.pokemonapp.data
 
+import android.annotation.SuppressLint
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+@SuppressLint("UnsafeOptInUsageError")
 @Serializable
 data class PokemonListResponse(
     val results: List<PokemonResult>
 )
 
+@SuppressLint("UnsafeOptInUsageError")
 @Serializable
 data class PokemonResult(
     val name: String,
     val url: String
 )
 
+@SuppressLint("UnsafeOptInUsageError")
 @Serializable
 data class PokemonDetail(
     val id: Int,
@@ -25,22 +29,26 @@ data class PokemonDetail(
     val types: List<TypeSlot> = emptyList()
 )
 
+@SuppressLint("UnsafeOptInUsageError")
 @Serializable
 data class Sprites(
     @SerialName("front_default") val frontDefault: String? = null,
     val other: OtherSprites? = null
 )
 
+@SuppressLint("UnsafeOptInUsageError")
 @Serializable
 data class OtherSprites(
     @SerialName("official-artwork") val officialArtwork: OfficialArtwork? = null
 )
 
+@SuppressLint("UnsafeOptInUsageError")
 @Serializable
 data class OfficialArtwork(
     @SerialName("front_default") val frontDefault: String? = null
 )
 
+@SuppressLint("UnsafeOptInUsageError")
 @Serializable
 data class TypeSlot(
     val slot: Int,
